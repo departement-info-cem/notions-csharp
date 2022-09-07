d24dc61b33bba6f3ea4db02d4d30cdee889f64d4
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2022-09-07 16:56:20 -0400

    Accueil

---
+++ docs/accueil.md
@@ -0,0 +1,17 @@
+---
+sidebar_position: 1
+---
+
+# Accueil
+
+Ce site est destiné aux étudiants du Cégep Édouard-Montpetit qui abordent l'apprentissage de la programmation en utilisant le langage C#. Il s'adresse particulièrement aux étudiants inscrits aux cours "420-116-CEM Algorithmie et programmation" et "420-CEB-CEM  Informatique pratique".
+
+Les objets de connaissance du langage C# utilisés dans ces cours s'y trouvent.
+
+Une section du site est consacrée aux composants graphiques usuels (GUI) et à la librairie VisualArrays développée par Stéphane Desmeules, professeur au département d'informatique.
+
+Ce site a été développé pendant la session d'automne 2010 par Francine Béliveau, professeure au département d'informatique.  Depuis ce temps, il est maintenu à jour par Claire Breton, professeure au département informatique.
+ 
+Pour toutes questions ou suggestions, communiquez avec Stéphane Desmeules à l'adresse courriel : stephane.desmeules@cegepmontpetit.ca
+
+Bon apprentissage !
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Notions C#',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
@@ -57,15 +57,15 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Notions C#',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Logo CEM',
+          src: 'img/logo.ico',
         },
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'accueil',
             position: 'left',
             label: 'Tutorial',
           },
@@ -84,8 +84,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Accueil',
+                to: '/docs/accueil',
               },
             ],
           },
