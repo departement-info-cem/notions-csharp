9f49e44cd9f11b81001102f5ee4ed9334fdc1402
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2022-09-07 22:49:39 -0400

    Support C#

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -120,11 +120,12 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Notions C#. CÉGEP Édouard-Montpetit.`,
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+        additionalLanguages: ['csharp'],
       },
     }),
 };
