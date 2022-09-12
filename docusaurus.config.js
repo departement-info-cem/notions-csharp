973543f4fda8d7ff2230d8cc32cfe1043be6ff10
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2022-09-12 15:56:03 -0400

    Fix broken link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,7 +88,7 @@ const config = {
             items: [
               {
                 label: 'Accueil',
-                to: '/docs/accueil',
+                to: '/',
               },
             ],
           },
