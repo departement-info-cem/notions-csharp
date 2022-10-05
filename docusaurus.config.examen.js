2d65347acf88e08fbd0b453495f863b5948afbc1
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2022-10-05 18:17:07 -0400

    Recherche hors-ligne examens

--- docusaurus.config.examen.js
+++ docusaurus.config.examen.js
@@ -16,6 +16,8 @@ const config = {
 
   trailingSlash: false,
 
+  plugins: [require.resolve('docusaurus-lunr-search')],
+  
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
